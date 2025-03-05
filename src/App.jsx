b9d79b368de30b9469474baedc9b3d3463c06b35
HoeumSenghun <hoeumsenghun369@gmail.com>
2025-03-05 13:52:02 +0700

    config layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import "./App.css";
+import AssignmentsComponent from "./components/AssignmentsComponent";
+import CardComponent from "./components/CardComponent";
+import DashboardComponent from "./components/DashboardComponent";
+import LearningMaterialsComponent from "./components/LearningMaterialsComponent";
+import SideBarComponent from "./components/SideBarComponent";
+import TopNavbarComponent from "./components/TopNavbarComponent";
 
 function App() {
   return (
     <>
-      <div>React Homework 004</div>
+      <div className="grid grid-cols-12">
+        <div className="col-span-2"><SideBarComponent /></div>
+        <div className="col-span-10 p-5 bg-gray-200">
+          <div><TopNavbarComponent /></div>
+          <div className="grid grid-cols-10 mt-5">
+            <div className="col-span-8">
+                <div><DashboardComponent /></div>
+                <div className="mt-5"><AssignmentsComponent /></div>
+                <div className="mt-7"><CardComponent /></div>
+            </div>
+            <div className="col-span-2"><LearningMaterialsComponent /></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
