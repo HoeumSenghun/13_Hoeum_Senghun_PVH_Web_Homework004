4c94cf81940fd3ee8758b04ecb376009aaa7fe5b
HoeumSenghun <hoeumsenghun369@gmail.com>
2025-03-05 14:26:41 +0700

    map learningMaterials

--- src/components/LearningMaterialsComponent.jsx
+++ src/components/LearningMaterialsComponent.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Star } from "lucide-react";
+import { Key, Star } from "lucide-react";
 import FilterComponent from "./FilterComponent";
+import { learningMaterials } from "../data/learningMaterials";
 
 export default function LearningMaterialsComponent() {
   return (
@@ -15,11 +16,13 @@ export default function LearningMaterialsComponent() {
       </div>
 
       {/* materials list */}
-      <div className="space-y-3">
-        <div className="bg-light-gray px-4 py-2 flex gap-5 items-center">
+      <div className="space-y-3 p-4">
+        {learningMaterials.map((material) => (
+          
+          <div key={material.id} className="bg-light-gray px-4 py-2 flex gap-5 items-center">
           <img
-            src="https://i.pinimg.com/736x/ca/e1/b4/cae1b4f6b223fe5a7bb712b680cffa67.jpg"
-            alt="HTML5"
+            src={material.image}
+            alt={material.title}
             width={50}
             height={50}
             className="rounded-xl"
@@ -27,12 +30,13 @@ export default function LearningMaterialsComponent() {
 
           <div className="w-full">
             <div className="flex justify-between">
-              <p className="text-base font-medium">HTML5</p>
-              <Star size={20} />
+              <p className="text-base font-medium">{material.title}</p>
+              <Star size={20} className={material.isFavorite ? "text-yellow-500" : "text-gray-400"} />
             </div>
-            <p className="text-gray-400 text-sm">Posted at: 2025/01/13</p>
+            <p className="text-gray-400 text-sm">Posted at: {material.postedAt}</p>
           </div>
         </div>
+        ))}
       </div>
     </div>
   );
