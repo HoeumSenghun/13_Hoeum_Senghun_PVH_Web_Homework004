1fbe4871e5d225bf24e19929e9fd88ed221e81de
HoeumSenghun <hoeumsenghun369@gmail.com>
2025-03-07 14:08:21 +0700

    handle AddNewProject

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import AddNewProjectComponent from "./components/AddNewProjectComponent";
 import AssignmentsComponent from "./components/AssignmentsComponent";
@@ -8,20 +9,30 @@ import SideBarComponent from "./components/SideBarComponent";
 import TopNavbarComponent from "./components/TopNavbarComponent";
 
 function App() {
+
+  const [projects, setProjects] = useState([]);
+  const handleAddProject = (newProject) =>{ setProjects ([...projects, newProject])};
+
   return (
     <>
-      <div className="grid grid-cols-12">
+      <div className="overflow-hidden grid grid-cols-12">
         <div className="col-span-2"><SideBarComponent /></div>
         <div className="col-span-10 p-5 bg-gray-200">
           <div><TopNavbarComponent /></div>
           <div className="grid grid-cols-10 mt-5">
             <div className="col-span-8">
-                <div><DashboardComponent /></div>
+                <div><DashboardComponent projects={projects} /></div>
                 <div className="mt-5 flex justify-between pr-18">
                   <AssignmentsComponent />
-                  <AddNewProjectComponent/>
+                  <AddNewProjectComponent onAddProject={handleAddProject}/>
                 </div>
-                <div className="mt-7"><CardComponent /></div>
+                <div className="mt-7 grid grid-cols-3 w-6xl overflow-y-auto h-[560px]">
+                {projects.map((project, index) => (
+                  <div className="p-4">
+                    <CardComponent key={index} project={project} />
+                  </div>
+                ))}
+              </div>
             </div>
             <div className="col-span-2"><LearningMaterialsComponent /></div>
           </div>
